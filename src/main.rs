760b72874846dc17d0617bb33bdf7caff502d82b
Harsh Shandilya <msfjarvis@gmail.com>
2019-09-28 17:07:01 +0530

    Flesh out implementation some more
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,34 @@ extern crate reqwest;
 
 mod source;
 
-use source::Source;
 use source::Download;
+use source::Source;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 
 fn main() {
-    let sources = vec![
-        Source {
-            name: String::from("Harsh Shandilya's hosts list"),
-            url: String::from("https://download.msfjarvis.website/adblock/hosts")
-        }
-    ];
+    let sources = vec![Source {
+        name: String::from("Harsh Shandilya's hosts list"),
+        url: String::from("https://download.msfjarvis.website/adblock/hosts"),
+    }];
     let mut hosts = String::new();
+    let mut dnsmasq_addrs = String::new();
     for source in sources {
         hosts.push_str(source.download_to_string().unwrap().as_str());
     }
-    println!("{}", hosts);
+    for line in hosts.lines() {
+        let next = line.chars().next();
+        if next == None || next.unwrap() == '#' {
+            continue;
+        }
+        for section in line.split_whitespace() {
+            if section == "0.0.0.0" || section == "127.0.0.1" {
+                continue;
+            }
+            dnsmasq_addrs.push_str(format!("server=/{}/\n", section).as_str())
+        }
+    }
+    let write_file = File::create("adblock.list").unwrap();
+    let mut writer = BufWriter::new(&write_file);
+    let _ = write!(&mut writer, "{}", dnsmasq_addrs);
 }
