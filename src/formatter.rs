db96dcb4e2d941b5c311ca57ce601a8a58beeb60
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-07 21:03:35 +0530

    Migrate formatting to a separate module
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ src/formatter.rs
@@ -0,0 +1,32 @@
+use crate::Source;
+use regex::Regex;
+
+pub trait Formatter {
+    // I don't fully understand this and would appreciate help :)
+    const LOCALHOST_ADDRS: &'static [&'static str] = &[
+        "localhost",
+        "local",
+        "localhost.localdomain",
+        "broadcasthost",
+    ];
+    fn format(&self) -> Vec<String>;
+}
+
+impl Formatter for Source {
+    fn format(&self) -> Vec<String> {
+        let re = Regex::new(r"^\s*(\d+\.\d+\.\d+\.\d+)\s+([\w\-\.]+)").unwrap();
+        let mut output: Vec<String> = vec![];
+        let raw_hosts = self.download_to_string().unwrap();
+        for line in raw_hosts.lines() {
+            if !re.is_match(line) {
+                continue;
+            }
+            for cap in re.captures_iter(line) {
+                if !Self::LOCALHOST_ADDRS.contains(&&cap[2]) {
+                    output.push(format!("server=/{}/", &cap[2]))
+                }
+            }
+        }
+        output
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,22 @@
 extern crate regex;
 extern crate reqwest;
 
+mod formatter;
 mod source;
 
-use regex::Regex;
+use formatter::Formatter;
 use source::Source;
 use std::fs::File;
 use std::io::{BufWriter, Write};
-
-// I don't fully understand this and would appreciate help :)
-const LOCALHOST_ADDRS: &[&str] = &[
-    "localhost",
-    "local",
-    "localhost.localdomain",
-    "broadcasthost",
-];
 
 fn main() {
     let sources = vec![Source {
         name: String::from("Harsh Shandilya's hosts list"),
         url: String::from("https://download.msfjarvis.website/adblock/hosts"),
     }];
-    let re = Regex::new(r"^\s*(\d+\.\d+\.\d+\.\d+)\s+([\w\-\.]+)").unwrap();
-    let mut hosts = String::new();
-    let mut dnsmasq_addrs = String::new();
-    for source in sources {
-        hosts.push_str(source.download_to_string().unwrap().as_str());
-    }
-    for line in hosts.lines() {
-        if !re.is_match(line) {
-            continue;
-        }
-        for cap in re.captures_iter(line) {
-            if !LOCALHOST_ADDRS.contains(&&cap[2]) {
-                dnsmasq_addrs.push_str(format!("server=/{}/\n", &cap[2]).as_str())
-            }
-        }
-    }
     let write_file = File::create("adblock.list").unwrap();
     let mut writer = BufWriter::new(&write_file);
-    match write!(&mut writer, "{}", dnsmasq_addrs) {
+    match write!(&mut writer, "{}", sources[0].format().join("\n")) {
         Ok(_) => {}
         Err(e) => println!("{}", e),
     };
