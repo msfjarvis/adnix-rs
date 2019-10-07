99f92e5feee5dfd0ed7c8560995d59be132aaf54
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-08 00:22:31 +0530

    Add the beginnings of a CLI program
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+extern crate clap;
 extern crate regex;
 extern crate reqwest;
 
 mod source;
 
+use clap::{App, Arg};
 use source::Source;
 use std::fs::File;
 use std::io::{BufWriter, Write};
@@ -12,7 +14,20 @@ fn main() {
         name: String::from("Harsh Shandilya's hosts list"),
         url: String::from("https://download.msfjarvis.website/adblock/hosts"),
     }];
-    let write_file = File::create("adblock.list").unwrap();
+    let matches = App::new("adnix-rs").version("0.1.0")
+        .author("Harsh Shandilya <msfjarvis@gmail.com>")
+        .about("CLI tool to convert ad blocking hosts files into DNSMasq or Unbound configuration files")
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("file")
+                .value_name("OUTPUT")
+                .help("Output file")
+                .default_value("adblock.list")
+                .takes_value(true),
+        )
+        .get_matches();
+    let write_file = File::create(matches.value_of("output").unwrap_or_default()).unwrap();
     let mut contents = String::new();
     let mut writer = BufWriter::new(&write_file);
     for source in sources {
