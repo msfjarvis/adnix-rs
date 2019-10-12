8090a5a82339be42c9187aa62e1aa9564bf5cfda
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-12 17:05:30 +0530

    Directly initialize sources HashMap
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,18 @@ fn main() {
     let yaml = load_yaml!("adnix.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let sources_file = if matches.is_present("sources_file") {
-        matches.value_of("sources_file").unwrap()
+    let sources: HashMap<String, Source> = if matches.is_present("sources_file") {
+        parse_file(&matches.value_of("sources_file").unwrap())
     } else {
-        ""
+        let mut list: HashMap<String, Source> = HashMap::new();
+        list.insert(
+            String::from("Harsh Shandilya's hosts list"),
+            Source {
+                url: String::from("https://download.msfjarvis.website/adblock/hosts"),
+            },
+        );
+        list
     };
-
-    let sources: HashMap<String, Source> = parse_file(&sources_file);
 
     let mut contents = String::new();
     let ipv4_addr = matches.value_of("ipv4_addr").unwrap_or_default();
@@ -57,31 +62,22 @@ fn main() {
 fn parse_file(filepath: &str) -> HashMap<String, Source> {
     println!("Sources path {}", filepath);
     let mut list: HashMap<String, Source> = HashMap::new();
-    if filepath != "" {
-        let file = File::open(filepath).unwrap_or_else(|err| {
-            eprintln!("Problem openning file: {}", err);
+    let file = File::open(filepath).unwrap_or_else(|err| {
+        eprintln!("Problem openning file: {}", err);
+        process::exit(1);
+    });
+
+    let lines = BufReader::new(file).lines();
+    for line in lines {
+        let content = line.unwrap_or_else(|err| {
+            eprintln!("Problem parsing lines: {}", err);
             process::exit(1);
         });
-
-        let lines = BufReader::new(file).lines();
-        for line in lines {
-            let content = line.unwrap_or_else(|err| {
-                eprintln!("Problem parsing lines: {}", err);
-                process::exit(1);
-            });
-            let vec: Vec<&str> = content.split('|').collect();
-            list.insert(
-                vec[0].to_owned(),
-                Source {
-                    url: vec[1].to_owned(),
-                },
-            );
-        }
-    } else {
+        let vec: Vec<&str> = content.split('|').collect();
         list.insert(
-            String::from("Harsh Shandilya's hosts list"),
+            vec[0].to_owned(),
             Source {
-                url: String::from("https://download.msfjarvis.website/adblock/hosts"),
+                url: vec[1].to_owned(),
             },
         );
     }
