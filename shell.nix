2df4fb2efed4632c0b462ab9d80b186f23b29659
Harsh Shandilya <me@msfjarvis.dev>
2022-06-17 12:40:47 +0530

    nix: switch to stable Rust

--- shell.nix
+++ shell.nix
@@ -1,11 +1,17 @@
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
-    (rust-bin.selectLatestNightlyWith (toolchain:
-      toolchain.default.override {
-        extensions = [ "rust-src" "rustfmt-preview" ];
-      }))
-  ] ++ lib.optionals stdenv.isDarwin [
-    pkgs.darwin.apple_sdk.frameworks.Security
-  ];
+    (rust-bin.stable.latest.default.override {
+      extensions = [
+        "clippy"
+        "llvm-tools-preview"
+        "rust-src"
+        "rustc-dev"
+        "rustfmt-preview"
+      ];
+      targets = lib.optionals stdenv.isDarwin [ "aarch64-apple-darwin" ]
+        ++ lib.optionals stdenv.isLinux [ "x86_64-unknown-linux-gnu" ];
+    })
+  ] ++ lib.optionals stdenv.isDarwin
+    [ pkgs.darwin.apple_sdk.frameworks.Security ];
 }
