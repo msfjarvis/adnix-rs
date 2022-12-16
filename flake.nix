3cc599a456c43820e634b0732c96bd79e062daec
Harsh Shandilya <me@msfjarvis.dev>
2022-12-16 06:34:17 +0000

    feat(nix): migrate to Nix flakes

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "adnix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, crane, flake-utils, advisory-db, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustStable =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
+        src = ./.;
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs; };
+        buildInputs = [ ];
+
+        adnix = craneLib.buildPackage {
+          inherit src;
+          doCheck = false;
+        };
+        adnix-clippy = craneLib.cargoClippy {
+          inherit cargoArtifacts src buildInputs;
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        };
+        adnix-fmt = craneLib.cargoFmt { inherit src; };
+        adnix-audit = craneLib.cargoAudit { inherit src advisory-db; };
+        adnix-nextest = craneLib.cargoNextest {
+          inherit cargoArtifacts src buildInputs;
+          partitions = 1;
+          partitionType = "count";
+        };
+      in {
+        checks = {
+          inherit adnix adnix-audit adnix-clippy adnix-fmt adnix-nextest;
+        };
+
+        packages.default = adnix;
+
+        apps.default = flake-utils.lib.mkApp { drv = adnix; };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks;
+
+          nativeBuildInputs = with pkgs; [
+            cargo-nextest
+            cargo-release
+            rustStable
+          ];
+        };
+      });
+}
