5f19c384793f36507c2a413854fba4967abf7b0f
Harsh Shandilya <me@msfjarvis.dev>
2024-12-16 18:33:24 +0530

    chore: reformat all files with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -22,82 +22,101 @@
   inputs.flake-compat.url = "github:nix-community/flake-compat";
   inputs.flake-compat.flake = false;
 
-  outputs = {
-    self,
-    nixpkgs,
-    advisory-db,
-    crane,
-    devshell,
-    fenix,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [devshell.overlays.default];
-      };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      advisory-db,
+      crane,
+      devshell,
+      fenix,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
 
-      rustStable = (import fenix {inherit pkgs;}).fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
-      };
+        rustStable = (import fenix { inherit pkgs; }).fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+        };
 
-      craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
-      commonArgs = {
-        src = craneLib.cleanCargoSource ./.;
-        buildInputs = [];
-        nativeBuildInputs = [];
-        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-      };
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = [ ];
+          nativeBuildInputs = [ ];
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+        };
 
-      cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {doCheck = false;});
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { doCheck = false; });
 
-      adnix = craneLib.buildPackage (commonArgs
-        // {
-          doCheck = false;
-        });
-      adnix-clippy = craneLib.cargoClippy (commonArgs
-        // {
-          inherit cargoArtifacts;
-        });
-      adnix-fmt = craneLib.cargoFmt (commonArgs // {});
-      adnix-audit = craneLib.cargoAudit (commonArgs // {inherit advisory-db;});
-      adnix-nextest = craneLib.cargoNextest (commonArgs
-        // {
-          inherit cargoArtifacts;
-          src = ./.;
-          partitions = 1;
-          partitionType = "count";
-        });
-    in {
-      checks = {
-        inherit adnix adnix-audit adnix-clippy adnix-fmt adnix-nextest;
-      };
-
-      packages.default = adnix;
-
-      apps.default = flake-utils.lib.mkApp {drv = adnix;};
-
-      devShells.default = pkgs.devshell.mkShell {
-        bash = {interactive = "";};
-
-        env = [
-          {
-            name = "DEVSHELL_NO_MOTD";
-            value = 1;
+        adnix = craneLib.buildPackage (
+          commonArgs
+          // {
+            doCheck = false;
           }
-        ];
+        );
+        adnix-clippy = craneLib.cargoClippy (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+        adnix-fmt = craneLib.cargoFmt (commonArgs // { });
+        adnix-audit = craneLib.cargoAudit (commonArgs // { inherit advisory-db; });
+        adnix-nextest = craneLib.cargoNextest (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            src = ./.;
+            partitions = 1;
+            partitionType = "count";
+          }
+        );
+      in
+      {
+        checks = {
+          inherit
+            adnix
+            adnix-audit
+            adnix-clippy
+            adnix-fmt
+            adnix-nextest
+            ;
+        };
 
-        packages = with pkgs; [
-          cargo-dist
-          cargo-nextest
-          cargo-release
-          fenix.packages.${system}.rust-analyzer
-          oranda
-          rustStable
-          stdenv.cc
-        ];
-      };
-    });
+        packages.default = adnix;
+
+        apps.default = flake-utils.lib.mkApp { drv = adnix; };
+
+        devShells.default = pkgs.devshell.mkShell {
+          bash = {
+            interactive = "";
+          };
+
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+
+          packages = with pkgs; [
+            cargo-dist
+            cargo-nextest
+            cargo-release
+            fenix.packages.${system}.rust-analyzer
+            oranda
+            rustStable
+            stdenv.cc
+          ];
+        };
+      }
+    );
 }
